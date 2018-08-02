8ef41b0d983f77d2cb6ba8eefc7d500d8ce5a788
Ware, Chris (L&W, Black mountain) <Chris.Ware@csiro.au>
2018-08-02 15:52:28 +1000

    init

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='maclab',
+      version='0.1',
+      description='Macroecology tools',
+      url='http://github.com/cwarecsiro/maclab',
+      author='Chris Ware',
+      author_email='chris.ware@csiro.au',
+      license='NA',
+      packages=['maclab'],
+      install_requires=[
+          'rasterio',
+          'warnings',
+          'jenkspy',
+          'numpy',
+          'operator',
+          'functools'
+      ]
+      zip_safe=False)
